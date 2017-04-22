070f6122909ef9fffd1564995f2204374745e78b
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-04-22 13:05:45 -0400

    added jquery min file
    q

--- js/main.js
+++ js/main.js
@@ -3,5 +3,27 @@
 
 $(document).ready(function(){
 
-  
+     $(".quote").text("I'm not your guy, Pal!");
+
+    // $('.buttons').on('click', 'button', function() {
+    //   alert("helllllllo");
+    // });
+
+
+     $('.buttons').on('click', '.refresh', function() {
+       $.ajax('http://quotesondesign.com/api/3.0/api-3.0.json?callback=my_function', {
+         success: function() {
+           $(".quote").text("quote");
+           alert("it worked");
+         }
+       });
+     });
+
 });
+
+
+
+
+// $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+//  $(.quote).append(a[0].content + "<p>— " + a[0].title + "</p>")
+// });
