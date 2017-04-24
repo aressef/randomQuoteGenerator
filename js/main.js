5f8eb60ce44eb36ef103194371180669e87eac36
Ryan Fuhrman <rsfuhrman10@gmail.com>
2017-04-23 23:18:48 -0400

    moved css back out and still working on getting new quote button to work

--- js/main.js
+++ js/main.js
@@ -1,34 +1,41 @@
-function ajaxCall() {
+function getQuote() {
   $.ajax({
-      url: 'http://quotesondesign.com/api/3.0/api-3.0.json',
+      url: 'http://quotesondesign.com/api/3.0/api-3.0.json?p=rand',
       dataType: 'jsonp',
       callback: 'callback',
       type: 'GET',
       success: function (data) {
-        var post = data.shift();
         document.getElementById('quote').innerHTML="\"" + data.quote + "\"";
         document.getElementById('author').innerHTML="-" + data.author;
-        document.getElementById('newQuote').addEventListener('click',function() {
-          ajaxCall();
-        });
-      }
+        console.log(data);
+      },
+      cache: false
   });
 }
 
+getQuote();
 
-ajaxCall();
 
+$('#newQuote').on('click', function(e) {
+  e.preventDefault();
+  getQuote();
+});
 
-// $('.newQuote').on('click', function(e) {
-//   e.preventDefault();
-//   $.ajax( {
-//     url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-//     success: function(data) {
-//       var post = data.shift(); // The data is an array of posts. Grab the first one.
-//       $('#quote').text(post.title);
-//       $('#author').html(post.content);
-//       console.log('hello');
-//     },
-//     cache: false
+// $(document).ready(function() {
+//   $('#newQuote').on('click', function(e) {
+//     e.preventDefault();
+//     $.ajax( {
+//       url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+//       dataType: 'jsonp',
+//       callback: 'callback',
+//       type: 'GET',
+//       success: function(data) {
+//         var post = data.shift(); // The data is an array of posts. Grab the first one.
+//         $('#author').text(post.title);
+//         $('#quote').html(post.content);
+//         console.log(post.title);
+//       },
+//       cache: false
+//     });
 //   });
 // });
